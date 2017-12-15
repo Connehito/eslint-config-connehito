745396d918b6237594488f2c9ccf54a0a45e8ee4
Ryo Adachi <ar.19920223@gmail.com>
2017-12-15 18:46:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# eslint-config-connehito
+
+ESLint shareable config for Connehito
+
+## Installation
+
+```sh
+$ yarn add --dev \
+  babel-eslint \
+  eslint \
+  eslint-config-prettier \
+  eslint-plugin-flowtype \
+  eslint-plugin-import \
+  eslint-plugin-prettier \
+  eslint-plugin-react \
+  prettier \
+  @connehito/eslint-config
+```
+
+## Usage
+
+Add this to your `.eslintrc` file:
+
+```json
+{
+  "extends": "@connehito"
+}
+```
+
+## License
+
+MIT © [Connehito](https://connehito.com)
---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  plugins: ['flowtype', 'import', 'prettier', 'react'],
+  extends: [
+    'plugin:flowtype/recommended',
+    'plugin:import/errors',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react'
+  ],
+  rules: {
+    'prettier/prettier': [
+      2,
+      {
+        printWidth: 120,
+        semi: false,
+        singleQuote: true
+      }
+    ]
+  }
+}
