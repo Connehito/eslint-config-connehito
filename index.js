a3cf66b1e2fec0bc797b808e415ed5b092f096d3
dachi <ar.19920223@gmail.com>
2020-12-16 01:56:27 +0900

    :wrench: Update rules

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 module.exports = {
+  root: true,
   env: {
+    es6: true,
     browser: true,
     node: true
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: 'module',
-    // Path of `tsconfig.json`.
-    // ex. `project: './tsconfig.json'`
-    project: './tsconfig.json',
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     ecmaFeatures: {
       jsx: true
-    }
+    },
+    sourceType: 'module'
   },
-  plugins: ['import', 'prettier', 'react'],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'jest',
+    'prettier',
+    'react'
+  ],
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
@@ -27,24 +34,48 @@ module.exports = {
   settings: {
     react: {
       version: 'detect'
-    }
+    },
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.ts',
+      '.tsx'
+    ]
   },
   rules: {
-    'prettier/prettier': [
-      2,
-      {
-        printWidth: 120,
-        semi: false,
-        singleQuote: true
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': 'off',
+    'sort-imports': 'off',
+    'import/order': ['error', {
+      alphabetize: {
+        order: 'asc'
       }
-    ]
+    }],
+    'prettier/prettier': ['error', {
+      printWidth: 120,
+      semi: false,
+      singleQuote: true
+    }]
   },
   overrides: [
     {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off'
+      }
+    },
+    {
+      files: ['**/__tests__/*'],
       env: {
-        jest: true
+        'jest/globals': true
       },
-      files: ['*.test.ts', '*.test.tsx']
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest/style'
+      ]
     }
   ]
 }
